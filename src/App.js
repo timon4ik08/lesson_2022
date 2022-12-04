dad1098e00d9be8363b706bd2a4f49c17a84697c
timon4ik08 <sysoevartem08@gmail.com>
2022-12-05 00:02:29 +0400

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className={"app-wrapper"}>
-            <header className={"header"}>
-                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"} alt={"logo"}/>
-            </header>
-            <nav className={"nav"}>
-                <div>Profile</div>
-                <div>Message</div>
-                <div>News</div>
-                <div>Music</div>
-                <div>Settings</div>
-            </nav>
-            <div className={"content"}>
-                <img src={"https://media.istockphoto.com/id/1327276218/photo/the-picturesque-mountain-landscape-on-the-sunset-background.jpg?b=1&s=170667a&w=0&k=20&c=8tzAabsLYLhMW6iDoeuBZUuaozi-F0lL2KcE49JVVaI="} alt={"font"}/>
-                My content
+        <div className={"container text-center"}>
+            <div className={"row align-items-start mt-4 mb-4 border"}>
+                <header className={"col"}>
+                    <img className={"mt-2 mb-2"}
+                         src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"}
+                         alt={"logo"}/>
+                </header>
+            </div>
+
+            <div className={"row"}>
+                <nav className={"nav col-2 p-5 border"}>
+                    <div className={"mx-auto"}>
+                       <a className={"btn btn-outline-primary nav-link"} href={"http://localhost:3000/"}>Profile</a><br/>
+                       <a className={"btn btn-outline-primary nav-link"} href={"http://localhost:3000/"}>Message</a><br/>
+                       <a className={"btn btn-outline-primary nav-link"} href={"http://localhost:3000/"}>News</a><br/>
+                       <a className={"btn btn-outline-primary nav-link"} href={"http://localhost:3000/"}>Music</a><br/>
+                       <a className={"btn btn-outline-primary nav-link"} href={"http://localhost:3000/"}>Settings</a><br/>
+                    </div>
+                </nav>
+
+                <div className={"content col p-0 ml-5 border"}>
+                    <img className={"fontContent"}
+                         src={"https://media.istockphoto.com/id/1327276218/photo/the-picturesque-mountain-landscape-on-the-sunset-background.jpg?b=1&s=170667a&w=0&k=20&c=8tzAabsLYLhMW6iDoeuBZUuaozi-F0lL2KcE49JVVaI="}
+                         alt={"font"}/>
+                    My content
+                </div>
             </div>
         </div>
     );
